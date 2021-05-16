0418769cf42c34106fd5a9ad4c1d146b878dd8e1
gabriel.miranda <gabriel.alan32@gmail.com>
2021-05-16 12:48:33 -0300

    Ajustes de Tela agendamento e exibição do banco

--- app/src/main/java/com/example/barbeariahgg/Activity/Appointment.java
+++ app/src/main/java/com/example/barbeariahgg/Activity/Appointment.java
@@ -4,16 +4,26 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.barbeariahgg.MainActivity;
 import com.example.barbeariahgg.R;
+import com.example.barbeariahgg.model.AgendamentoModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,73 +34,121 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Locale;
 
 public class Appointment extends AppCompatActivity {
 
-    private DatePicker date_agendamento;
+    private Button buttonDatePicker;
     private Button  btn_agendar;
     private RadioGroup rg_grupo;
-    private ListView lst_horarios;
+    private Spinner spinner_horario;
+    private DatePickerDialog datePickerDialog;
+    private ArrayList<String> list ;
+    private ArrayAdapter<String> adapter ;
+
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        final ArrayList<String> list = new ArrayList<>();
-        final ArrayList<String> list2 = new ArrayList<>();
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_horarios, list);
-        int dia;
-        int mes;
-        int ano;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
+        initDatePicker();
 
-        date_agendamento = findViewById(R.id.date_agendamento);
+        list = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this, R.layout.list_horarios, list);
+        initHorarios();
+        verificaDisponibilidade();
+        buttonDatePicker = findViewById(R.id.buttonDatePicker);
+        buttonDatePicker.setText(getTodaysDate());
         btn_agendar = findViewById(R.id.btn_agendar);
-        lst_horarios = findViewById(R.id.lst_horarios);
+        spinner_horario = findViewById(R.id.spinner_horario);
+        spinner_horario.setAdapter(adapter);
 
-        lst_horarios.setAdapter(adapter);
-
-        dia = date_agendamento.getDayOfMonth();
-        mes = date_agendamento.getMonth();
-        ano = date_agendamento.getYear();
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(ano, mes, dia);
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        String formatedDate = sdf.format(calendar.getTime());
-
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("horarios_disponiveis");
-        reference.addValueEventListener(new ValueEventListener() {
+        btn_agendar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
-                list.clear();
-                for(DataSnapshot snapshot : datasnapshot.getChildren()){
-                    list.add(snapshot.child("hora").getValue().toString());
-                }
-            }
+            public void onClick(View v) {
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                AgendamentoModel agendamento = new AgendamentoModel();
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                agendamento.setId_cliente(currentUser.getUid());
+                agendamento.setData(buttonDatePicker.getText().toString());
+                agendamento.setHorario(spinner_horario.getSelectedItem().toString());
 
+                agendamento.salvar();
+                Toast.makeText(Appointment.this, "Agendamento efetuado com sucesso!", Toast.LENGTH_SHORT).show();
+                abrirTelaPrincipal();
             }
         });
+    }
 
-        reference = FirebaseDatabase.getInstance().getReference().child("agendamento");
-        reference.addValueEventListener(new ValueEventListener() {
+    private void initHorarios() {
+        list.add("09:00");
+        list.add("10:00");
+        list.add("11:00");
+        list.add("12:00");
+        list.add("13:00");
+        list.add("14:00");
+        list.add("15:00");
+        list.add("16:00");
+        list.add("17:00");
+        list.add("18:00");
+    }
+
+    private void abrirTelaPrincipal() {
+        Intent intent = new Intent(Appointment.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private String getTodaysDate() {
+        Calendar calendar = Calendar.getInstance();
+        int ano = calendar.get(Calendar.YEAR);
+        int mes = calendar.get(Calendar.MONTH);
+        mes += 1;
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(dia,mes,ano);
+    }
+
+    public void initDatePicker(){
+
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = makeDateString(dayOfMonth,month,year);
+                buttonDatePicker.setText(date);
+                list.clear();
+                initHorarios();
+                verificaDisponibilidade();
+            }
+        };
+
+        Calendar calendar = Calendar.getInstance();
+        int ano = calendar.get(Calendar.YEAR);
+        int mes = calendar.get(Calendar.MONTH);
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, ano, mes, dia);
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+    }
+
+    private void verificaDisponibilidade() {
+        DatabaseReference referenceAgendamento = FirebaseDatabase.getInstance().getReference().child("agendamentos");
+        referenceAgendamento.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot datasnapshot) {
                 for(DataSnapshot snapshot : datasnapshot.getChildren()){
-                    if (formatedDate == snapshot.child("data").getValue().toString()){
-                     for(int i=0; i<list.size();i++){
-                        if (snapshot.child("horario").getValue().toString() == list.get(i) ){
-                            list.remove(i);
+                    if (buttonDatePicker.getText().toString().equals(snapshot.child("data").getValue().toString())){
+                        for (int i=0; i<list.size();i++){
+                            if (list.get(i).equals(snapshot.child("horario").getValue().toString())){
+                                list.remove(i);
+                            }
                         }
-                     }
                     }
                 }
                 Collections.sort(list);
@@ -102,7 +160,45 @@ public class Appointment extends AppCompatActivity {
 
             }
         });
+    }
 
+    private String makeDateString(int dayOfMonth, int month, int year) {
+        return dayOfMonth + " " + getMonthFormat(month) + " " + year;
+    }
 
+    private String getMonthFormat(int month) {
+        switch (month){
+            case 1:
+                return "JAN";
+            case 2:
+                return "FEV";
+            case 3:
+                return "MAR";
+            case 4:
+                return "ABR";
+            case 5:
+                return "MAI";
+            case 6:
+                return "JUN";
+            case 7:
+                return "JUL";
+            case 8:
+                return "AGO";
+            case 9:
+                return "SET";
+            case 10:
+                return "OUT";
+            case 11:
+                return "NOV";
+            case 12:
+                return "DEZ";
+
+            default:
+                return "JAN";
+        }
+    }
+
+    public void openDataPicker(View view) {
+        datePickerDialog.show();
     }
 }
--- app/src/main/java/com/example/barbeariahgg/MainActivity.java
+++ app/src/main/java/com/example/barbeariahgg/MainActivity.java
@@ -1,28 +1,79 @@
 package com.example.barbeariahgg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.barbeariahgg.Activity.Appointment;
 import com.example.barbeariahgg.Activity.Login;
 import com.example.barbeariahgg.Activity.Register;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private Button add;
+    private ListView lst_agendamentos;
+    private ArrayList<String> list;
+    String[] items = {"Cancelar Agendamento"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         add = findViewById(R.id.add);
+        lst_agendamentos = findViewById(R.id.lst_agendamentos);
+
+        list = new ArrayList<>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_agendamentos, list);
+//        final ArrayAdapter<String> adapterOpcoes = new ArrayAdapter<String>(this, R.layout.list_opcoes, items);
+
+        lst_agendamentos.setAdapter(adapter);
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("agendamentos");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                list.clear();
+                for(DataSnapshot snapshot : datasnapshot.getChildren()){
+                    if (currentUser.getUid().toString().equals(snapshot.child("id_cliente").getValue().toString())){
+                        list.add(snapshot.child("data").getValue().toString() +" : "+ snapshot.child("horario").getValue().toString());
+                    }
+                }
+                if (list.isEmpty()){
+                    list.add("Não existem agendamentos");
+                }
+
+                Collections.sort(list);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +83,30 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        registerForContextMenu(lst_agendamentos);
     }
+
+/*    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        if (v.getId() == R.id.lst_agendamentos){
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            String [] menuItems = items;
+            for (int i=0;i<menuItems.length;i++){
+                menu.add(Menu.NONE,i,i,menuItems[i]);
+            }
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int menuItemIndex = item.getItemId();
+        String [] menuItems = items;
+        String menuItemName = menuItems[menuItemIndex];
+    }*/
 
     @Override
     protected void onStart() {
